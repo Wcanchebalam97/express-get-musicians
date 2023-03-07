e3f5a0a80999b680e03c8e747d9dd0a318f389ff
Wcanchebalam97 <williamcanchejr@gmail.com>
2023-03-07 13:36:49 -0800

    Done with Day 2 Workshop

--- server.js
+++ server.js
@@ -5,9 +5,22 @@ const {sequelize} = require("./db")
 
 const port = 3000;
 
+
+
 //TODO
 
 app.listen(port, () => {
     sequelize.sync();
-    console.log(`Listening on port ${port}`)
+    console.log(`Server listening at http://localhost:${port}`)
+})
+
+app.get('/musicians', async (request, response) => {
+    const allMusicians = await Musician.findAll();
+    response.status(200).json(allMusicians);
+})
+
+app.get('/musicians/:id', async (request, response) => {
+    const id = request.params.id;
+    const foundMusician = await Musician.findByPk(id);
+    response.json(foundMusician);
 })
